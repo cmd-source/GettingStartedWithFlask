ac40964555a37db3a832018f07ee15092c64e749
cmd-source <conorcrosbie09@gmail.com>
2021-07-05 16:15:18 +0000

    adding in member.html page to extend the profiles

--- run.py
+++ run.py
@@ -10,12 +10,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
 
 @app.route("/about/<member_name>")
 def about_member(member_name):
@@ -25,11 +27,13 @@ def about_member(member_name):
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return "<h1>" + member["name"] + "</h1>"
+    return render_template("member.html", member = member)
+
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
+
 
 @app.route("/careers")
 def careers():
